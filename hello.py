da8713a83995ecc005dac968ab9f2eae1ceac251
vadimdne <vadim.tikanov@gmail.com>
2014-09-16 15:16:33 +0300

    added user name handler

--- hello.py
+++ hello.py
@@ -5,5 +5,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
