07baa19f3244775d5948f4b74fd9ec726b401ce1
vadimdne <vadim.tikanov@gmail.com>
2014-09-18 17:19:42 +0300

    working with tasks

---
+++ tasks.py
@@ -0,0 +1,43 @@
+TASKS = [
+    {
+        "owner": "vadim",
+        "title": "To complete python backend",
+        "date": "15-10-2014",
+        "isopen": "True",
+    },
+    {
+        "owner": "vadim",
+        "title": "Implement HTML markup",
+        "date": "13-10-2014",
+        "isopen": "True",
+    },
+    {
+        "owner": "vadim",
+        "title": "Connect to Database",
+        "date": "14-10-2014",
+        "isopen": "True",
+    },
+    {
+        "owner": "roman",
+        "title": "Teach python course",
+        "date": "15-10-2014",
+        "isopen": "True",
+    },
+    {
+        "owner": "roman",
+        "title": "Plant a tree",
+        "date": "13-10-2014",
+        "isopen": "True",
+    },
+    {
+        "owner": "roman",
+        "title": "Build a house",
+        "date": "14-10-2014",
+        "isopen": "True",
+    },
+]
+
+if __name__ == "__main__":
+    for task in TASKS:
+        print task
+
