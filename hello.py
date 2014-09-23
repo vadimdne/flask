5ee44f3ef769a6030dd3107ef1b5645b603d4453
vadimdne <vadim.tikanov@gmail.com>
2014-09-23 18:35:55 +0300

    add task possibility

--- hello.py
+++ hello.py
@@ -12,6 +12,16 @@ app = Flask(__name__)
 def login_form():
     return render_template('login_form.html')
 
+@app.route("/login", methods=['POST'])
+def login():
+    if request.form['username'] in USERS and USERS[request.form['username']] == request.form['password']:
+        response = make_response(redirect("/", code=302))
+        response.set_cookie('islogged', 'TRUE')
+        response.set_cookie('username', request.form['username'])
+        return response
+    else:
+        return redirect("/login/form", code=302)
+
 @app.route("/")
 def showtasks():
     if request.cookies.get('islogged'):
@@ -24,15 +34,21 @@ def showtasks():
     else:
         return redirect("/login/form", code=302)
 
-@app.route("/login", methods=['POST'])
-def login():
-    if request.form['username'] in USERS and USERS[request.form['username']] == request.form['password']:
-        response = make_response(redirect("/", code=302))
-        response.set_cookie('islogged', 'TRUE')
-        response.set_cookie('username', request.form['username'])
-        return response
-    else:
-        return redirect("/login/form", code=302)
+@app.route("/addtask/form")
+def add_task_form():
+    return render_template('add_task.html')
 
-#app.run(debug=True)
-app.run(host="0.0.0.0", port=80)
+@app.route("/addtask", methods=['POST'])
+def add_task():
+    new_task = {
+        "owner": request.cookies.get('username'),
+        "title": request.form['task_name'],
+        "date": request.form['task_date'],
+        "isopen": "True",
+    }
+    TASKS.append(new_task)
+    response = make_response(redirect("/", code=302))
+    return response
+
+app.run(debug=True)
+#app.run(host="0.0.0.0", port=80)
