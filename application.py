8048264aa951ba092098392de65e7ca212078874
vadim <vadim.tikanov@gmail.com>
2014-11-04 23:02:39 +0200

    deployment mode

--- application.py
+++ application.py
@@ -96,5 +96,5 @@ bind_controller(app=app,
                 methods=['GET'])
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    #app.run(host="0.0.0.0", port=80)
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=80)
